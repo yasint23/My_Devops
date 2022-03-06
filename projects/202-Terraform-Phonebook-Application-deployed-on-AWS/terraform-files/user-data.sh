5a3c871ebf81908833e33cb3fbe37acc3bad6b71
Yasin.T <87154797+yasint23@users.noreply.github.com>
2022-03-06 23:25:50 +0000

    Update user-data.sh

--- projects/202-Terraform-Phonebook-Application-deployed-on-AWS/terraform-files/user-data.sh
+++ projects/202-Terraform-Phonebook-Application-deployed-on-AWS/terraform-files/user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="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"
+TOKEN="xxxxxxxxxxxxx"
 cd /home/ec2-user && git clone https://$TOKEN@github.com/TylerCounter/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
